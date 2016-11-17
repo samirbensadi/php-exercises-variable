03e6d61506b4fbf1c20984ad32b73f6474c3b345
samir bensadi <samirbensadi@gmail.com>
2016-11-17 11:41:15 +0100

    done

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+//exercice 1 et 6:
+
+ 
+
+
+	$nom = "Samir";
+	echo "Bonjour". " " .$nom.", "."Comment vas tu ? "."<br>";
+
+
+
+//exercice 2 et 7:
+
+
+	$nom = "Ben sadi"; 
+	$prenom = "Samir"; 
+	$age = 30; 
+	
+	echo "Bonjour ".$nom. " " .$prenom. " " ."Tu as ".$age." ans"."<br>";
+
+//exercice 3: 
+$Km = 1; 
+
+echo "Tu as parcouru". $Km ."Km <br>"; 
+
+$Km = 3; 
+echo "Tu as parcouru". $Km ."Km <br>";
+
+$Km = 125; 
+echo "Tu as parcouru". $Km ."Km <br>";
+
+//exercice 4 :
+
+$chaine = "string";
+$entier = 30;
+$flot = 1.5;
+$boo = true;
+
+echo $chaine."<br>".$entier."<br>".$flot."<br>";
+var_dump($boo);
+
+
+//exercice 5 :
+echo "<br>";
+$nbr = null; 
+var_dump($nbr); 
+$nbr = 777;
+echo "<br>";
+var_dump($nbr);
+
+//exercice 8 :
+echo "<br>";
+$math1 = 3 + 4; 
+$math2 = 5 * 20;
+$math3 = 45 / 5 ;
+
+echo $math1."<br> ".$math2."<br> ".$math3;
+
+
+
+
+
+
+ ?>
